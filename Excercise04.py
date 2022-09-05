816b47f6b6f425b1f48655296295c3b87b9c90ad
Brandonbosquez <brandon.bosquezgonzal@metropolia.fi>
2022-09-05 10:24:45 +0300

    commit

--- Excercise04.py
+++ Excercise04.py
@@ -8,8 +8,13 @@ while n <= 1000 :
 #2
 print("Welcome to the Inches to Centimeters converter")
 inch = 1
+<<<<<<< HEAD
 print("If you want to end the program, enter a negative number")
 while inch >= 0 :
+=======
+while inch >= 0 :
+    print("If you want to end the program, enter a negative number")
+>>>>>>> 2501cfd (commit)
     inch = int(input("Please enter the amount of inches to be converted: "))
     centi = inch * 2.54
     if inch >= 0 :
@@ -17,3 +22,28 @@ while inch >= 0 :
 else:
     print("Program ended, goodbye")
 #3
+<<<<<<< HEAD
+=======
+print("Smallest an biggest number detector")
+number = input("Please enter a number: ")
+least = number
+greatest = number
+while number != "" :
+    if number > greatest :
+        greatest = number
+    if number < least :
+        least = number
+    print("To end the program enter an empty string (Press Enter)")
+    number = input("Please enter another number: ")
+    if str(number) == "" :
+        break
+print("Your greatest number is: ", greatest)
+print("Your smallest number is: ", least)
+print("Program ended")
+
+#4
+
+
+
+
+>>>>>>> 2501cfd (commit)
