169f9b74f4e9e5f8d93548271d2f7a42b57f4bef
brandonbosquez <brandon.bosquezgonzal@metropolia.fi>
2022-11-02 20:06:48 +0200

    Ex10

--- Practices/Hiekkalaatikko.py
+++ Practices/Hiekkalaatikko.py
@@ -1,58 +1,56 @@
 #Practicas de experimento caja de arena
 
-class Elevator:
-    def __init__(self, bottom, top):
-        self.bottom_floor = int(bottom)
-        self.top_floor = int(top)
-        self.current_floor = self.bottom_floor
+class Car4:
+    def __init__(self, reg_num, max_spe):
+        self.reg_num = reg_num
+        self.max_spe = max_spe
+        self.cur_spe = 0
+        self.dis_tra = 0
 
-    def floor_up(self):
-        if self.current_floor <= self.top_floor:
-            print("Elevator moves UP")
-            self.current_floor += 1
+    def accelerate(self, spe_cha):
+        acc_prueba = self.cur_spe + spe_cha
+        if acc_prueba > 0 and acc_prueba < self.max_spe:
+            self.cur_spe += spe_cha
+        elif acc_prueba > self.max_spe:
+            self.cur_spe = self.max_spe
+            print(
+                f"The car's speed cannot exceed the maximum speed of the car, the speed maxes out at {self.cur_spe} km/h ")
+        elif acc_prueba < 0:
+            self.cur_spe = 0
+            print(f"The car's speed cannot be less than 0,the car comes to a stop")
         else:
-            print("Elevator has reached the top floor")
+            print("Speed change cannot be made")
+        return self.cur_spe
 
-    def floor_down(self):
-        if self.current_floor >= self.bottom_floor:
-            print("Elevator moves DOWN")
-            self.current_floor += -1
-        else:
-            print("Elevator has reached the bottom floor")
+    def drive(self, time):
+        self.dis_tra += (time * self.cur_spe)
+        return self.dis_tra
 
-    def go_to_floor(self,floor_goal):
-        if floor_goal < self.bottom_floor:
-            return print("ERROR Elevator cannot go lower than bottom floor")
-        elif floor_goal > self.top_floor:
-            return print("ERROR Elevator cannot go higher than top floor")
-        while self.current_floor != floor_goal :
-            if floor_goal > self.current_floor:
-                self.floor_up()
 
-            elif floor_goal < self.current_floor:
-                self.floor_down()
+race_cars = []
 
-        if floor_goal == self.current_floor:
-            print(f"You are currently on your desired floor: {self.current_floor}")
-class Building(Elevator):
-    def __init__(self, bottom, top, elevators):
+for x in range(10):
+    x += 1
+    reg_num = "ABC-" + str(x)
+    max_spe = random.randint(100,200)
+    c = Car4(reg_num,max_spe)
+    race_cars.append(c)
+    print(f"Car {x} created")
 
-        self.num_of_elevators = int(elevators)
-        self.elevators= []
-        for i in range(int(self.num_of_elevators)):
-            i = super().__init__(bottom, top)
-            self.elevators.append(i)
+print("Cars ready for race")
+print("1, 2, 3 ..." "\nGOOO!!!")
 
-    def run_elevator(self, num, destin):
-        elevator_num = int(num) - 1
-        self.elevators[elevator_num] = super().go_to_floor(destin)
+while True:
+    for i in range(10):
+        acc_cha = random.randint(-10 , 15)
+        race_cars[i].accelerate(acc_cha)
+        race_cars[i].drive(1)
+    if race_cars[i].dis_tra > 10000:
+        print("Race ended")
+        break
 
-print("Let's create a building with different elevators")
-bot_floor = input("What will be the bottom floor?: ")
-top_floor = input("What will be the top floor?: ")
-num_elevators = input("How many elevators will the building have?: ")
+print(f"RACE RESULTS"
+      f"\nPlate     Max Speed      Current Speed       Distance Travelled       ")
 
-building1 = Building(bot_floor,top_floor,num_elevators)
-elevator_number = input("What elevator do you wish to move?: ")
-destination = int(input("Where should the elevator move?: "))
-building1.run_elevator(elevator_number, destination)
+for i in range(10):
+    print(f"{race_cars[i].reg_num}      {race_cars[i].max_spe}                {race_cars[i].cur_spe}                     {race_cars[i].dis_tra}")
